866b43a1831716b9936232e52c4f1b4608ecdd08
Cate Mackenzie <cate.c.mackenzie@gmail.com>
2019-12-01 16:28:13 -0800

    Adds implementations for vector length and length squared, moves non-member operator overloads to header

--- RayTracingWeekend/Vec3.cpp
+++ RayTracingWeekend/Vec3.cpp
@@ -2,6 +2,14 @@
 
 Vec3::Vec3(float x, float y, float z) : x(x), y(y), z(z) {}
 
+inline float Vec3::length() const {
+	return sqrt(x * x + y * y + z * z);
+}
+
+inline float Vec3::lengthSquared() const {
+	return x * x + y * y + z * z;
+}
+
 inline const Vec3& Vec3::operator+() const {
 	return *this;
 }
@@ -52,61 +60,9 @@ inline Vec3& Vec3::operator/=(const float t) {
 	return *this;
 }
 
-inline std::istream& operator>>(std::istream& is, Vec3 &t) {
-	is >> t.x >> t.y >> t.z;
-	return is;
-}
-
-inline std::ostream& operator<<(std::ostream& os, const Vec3& t) {
-	os << t.x << " " << t.y << " " << t.z;
-	return os;
-}
-
 inline void Vec3::makeUnit() {
 	float length = this->length();
 	x /= length;
 	y /= length;
 	z /= length;
-}
-
-inline Vec3 operator+(const Vec3& v1, const Vec3& v2) {
-	return Vec3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
-}
-
-inline Vec3 operator-(const Vec3& v1, const Vec3& v2) {
-	return Vec3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
-}
-
-inline Vec3 operator*(const Vec3& v1, const Vec3& v2) {
-	return Vec3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
-}
-
-inline Vec3 operator/(const Vec3 v1, const Vec3& v2) {
-	return Vec3(v1.x / v2.x, v1.y / v2.y, v1.z / v2.z);
-}
-
-inline Vec3 operator*(float t, const Vec3& v) {
-	return Vec3(t * v.x, t * v.y, t * v.z);
-}
-
-inline Vec3 operator/(Vec3 v, float t) {
-	return Vec3(v.x / t, v.y / t, v.z / t);
-}
-
-inline Vec3 operator*(const Vec3& v, float t) {
-	return Vec3(v.x * t, v.y * t, v.z * t);
-}
-
-inline float dot(const Vec3& v1, const Vec3& v2) {
-	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
-}
-
-inline Vec3 cross(const Vec3& v1, const Vec3& v2) {
-	return Vec3((v1.y * v2.z - v1.z * v2.y),
-		(-(v1.x * v2.z - v1.z * v2.x)),
-		(v1.x * v2.y - v1.y * v2.x));
-}
-
-inline Vec3 unitVector(Vec3 v) {
-	return v / v.length();
 }
--- RayTracingWeekend/Vec3.h
+++ RayTracingWeekend/Vec3.h
@@ -26,3 +26,54 @@ public:
 	float x, y, z;
 };
 
+inline std::istream& operator>>(std::istream& is, Vec3& t) {
+	is >> t.x >> t.y >> t.z;
+	return is;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Vec3& t) {
+	os << t.x << " " << t.y << " " << t.z;
+	return os;
+}
+
+inline Vec3 operator+(const Vec3& v1, const Vec3& v2) {
+	return Vec3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+}
+
+inline Vec3 operator-(const Vec3& v1, const Vec3& v2) {
+	return Vec3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+}
+
+inline Vec3 operator*(const Vec3& v1, const Vec3& v2) {
+	return Vec3(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z);
+}
+
+inline Vec3 operator/(const Vec3 v1, const Vec3& v2) {
+	return Vec3(v1.x / v2.x, v1.y / v2.y, v1.z / v2.z);
+}
+
+inline Vec3 operator*(float t, const Vec3& v) {
+	return Vec3(t * v.x, t * v.y, t * v.z);
+}
+
+inline Vec3 operator/(Vec3 v, float t) {
+	return Vec3(v.x / t, v.y / t, v.z / t);
+}
+
+inline Vec3 operator*(const Vec3& v, float t) {
+	return Vec3(v.x * t, v.y * t, v.z * t);
+}
+
+inline float dot(const Vec3& v1, const Vec3& v2) {
+	return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+inline Vec3 cross(const Vec3& v1, const Vec3& v2) {
+	return Vec3((v1.y * v2.z - v1.z * v2.y),
+		(-(v1.x * v2.z - v1.z * v2.x)),
+		(v1.x * v2.y - v1.y * v2.x));
+}
+
+inline Vec3 unitVector(Vec3 v) {
+	return v / v.length();
+}
